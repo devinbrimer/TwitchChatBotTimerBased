d80b6a0848c1ce6fe0a5299712cd1dfb0d571e7c
Devin Brimer <devinthetech@gmail.com>
2016-04-20 00:20:30 -0700

    Initial Commit

---
+++ TwitchChatBot001/TwitchChatBot001/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace TwitchChatBot001
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.lblServer = new System.Windows.Forms.Label();
+            this.rtbRoom = new System.Windows.Forms.RichTextBox();
+            this.lblRoom = new System.Windows.Forms.Label();
+            this.rtbServer = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // lblServer
+            // 
+            this.lblServer.AutoSize = true;
+            this.lblServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblServer.Location = new System.Drawing.Point(13, 13);
+            this.lblServer.Name = "lblServer";
+            this.lblServer.Size = new System.Drawing.Size(55, 18);
+            this.lblServer.TabIndex = 0;
+            this.lblServer.Text = "Server:";
+            // 
+            // rtbRoom
+            // 
+            this.rtbRoom.DetectUrls = false;
+            this.rtbRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbRoom.Location = new System.Drawing.Point(629, 34);
+            this.rtbRoom.Name = "rtbRoom";
+            this.rtbRoom.ReadOnly = true;
+            this.rtbRoom.Size = new System.Drawing.Size(443, 606);
+            this.rtbRoom.TabIndex = 1;
+            this.rtbRoom.Text = "";
+            // 
+            // lblRoom
+            // 
+            this.lblRoom.AutoSize = true;
+            this.lblRoom.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRoom.Location = new System.Drawing.Point(626, 13);
+            this.lblRoom.Name = "lblRoom";
+            this.lblRoom.Size = new System.Drawing.Size(54, 18);
+            this.lblRoom.TabIndex = 2;
+            this.lblRoom.Text = "Room:";
+            // 
+            // rtbServer
+            // 
+            this.rtbServer.DetectUrls = false;
+            this.rtbServer.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbServer.Location = new System.Drawing.Point(16, 34);
+            this.rtbServer.Name = "rtbServer";
+            this.rtbServer.ReadOnly = true;
+            this.rtbServer.Size = new System.Drawing.Size(443, 606);
+            this.rtbServer.TabIndex = 3;
+            this.rtbServer.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1084, 682);
+            this.Controls.Add(this.rtbServer);
+            this.Controls.Add(this.lblRoom);
+            this.Controls.Add(this.rtbRoom);
+            this.Controls.Add(this.lblServer);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label lblServer;
+        private System.Windows.Forms.RichTextBox rtbRoom;
+        private System.Windows.Forms.Label lblRoom;
+        private System.Windows.Forms.RichTextBox rtbServer;
+    }
+}
+
---
+++ TwitchChatBot001/TwitchChatBot001/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.IO;
+
+namespace TwitchChatBot001
+{
+    public partial class Form1 : Form
+    {
+        Queue<string> sendMessageQueue;
+        TcpClient tcpClient;
+        StreamReader reader;
+        StreamWriter writer;
+        string userName;
+        string passWord;
+        string channelName;
+        string chatMessagePrefix;
+        string chatCommandId;
+        DateTime lastMessageTime;
+
+        public Form1()
+        {
+            sendMessageQueue = new Queue<string>();
+            this.userName = "brimerbot";
+            this.passWord = File.ReadAllText("password.txt");
+            this.channelName = "devin_brimer";
+            chatCommandId = "PRIVMSG";
+            chatMessagePrefix = $":{userName}!{userName}@{userName}.tmi.twitch.tv {chatCommandId} #{channelName} :";
+            
+
+            InitializeComponent();
+            Reconnect();
+        }
+
+        void Reconnect()
+        {
+            tcpClient = new TcpClient("irc.twitch.tv", 6667);
+            reader = new StreamReader(tcpClient.GetStream());
+            writer = new StreamWriter(tcpClient.GetStream());
+            writer.AutoFlush = true;
+
+            writer.WriteLine("PASS " + passWord + Environment.NewLine
+                + "NICK " + userName + Environment.NewLine
+                + "USER " + userName + " 8 * :" + userName);
+            writer.WriteLine("CAP REQ :twitch.tv/membership");
+            writer.WriteLine("JOIN #" + channelName);
+            //writer.Flush();
+            lastMessageTime = DateTime.Now;
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            if (!tcpClient.Connected)
+            {
+                Reconnect();
+            }
+
+            TryReceiveMessages();
+            TrySendingMessages();
+        }
+
+        void TrySendingMessages()
+        {
+            if (DateTime.Now - lastMessageTime > TimeSpan.FromSeconds(5))
+            {
+                if (sendMessageQueue.Count > 0)
+                {
+                    string outMessage = sendMessageQueue.Dequeue();
+
+                    writer.WriteLine($"{chatMessagePrefix}{outMessage}");
+
+                    string timeStamp = DateTime.Now.ToString("HH:mm");
+                    
+                    rtbRoom.AppendText($"\r\n<{timeStamp}> {userName} : {outMessage}");
+                    rtbRoom.ScrollToCaret();
+
+                    lastMessageTime = DateTime.Now;
+                }
+            }
+            
+        }
+
+        void TryReceiveMessages()
+        {
+            if (tcpClient.Available > 0 || reader.Peek() >= 0)
+            {
+                string timeStamp = DateTime.Now.ToString("HH:mm:ss");
+                string message = reader.ReadLine();
+
+                // Pure messages received from server to Server rtb
+                rtbServer.AppendText($"\r\n<{timeStamp}>{message}");
+                rtbServer.ScrollToCaret();
+
+                // if PING, then PONG
+                if (message.Equals("PING :tmi.twitch.tv"))
+                {
+                    writer.WriteLine("PONG :tmi.twitch.tv");
+                    rtbServer.AppendText($"\r\n<{timeStamp}>PONG :tmi.twitch.tv");
+                    rtbServer.ScrollToCaret();
+                }
+                else // check if this is a standard chat message
+                {
+                    int iCollon = message.IndexOf(":", 1);
+                    if (iCollon > 0)
+                    {
+                        string botCommand = message.Substring(1, iCollon);
+
+                        if (botCommand.Contains(chatCommandId))
+                        {
+                            int iBang = botCommand.IndexOf("!");
+                            if (iBang > 0)
+                            {
+                                string speaker = botCommand.Substring(0, iBang);
+                                string chatMessage = message.Substring(iCollon + 1);
+                                ReceiveMessage(speaker, chatMessage);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void ReceiveMessage(string speaker, string inMessage)
+        {
+            string timeStamp = DateTime.Now.ToString("HH:mm");
+            
+            rtbRoom.AppendText($"\r\n<{timeStamp}> {speaker} : {inMessage}");
+
+            if (inMessage.Equals("!hi"))
+            {
+                SendMessage($"Hello, {speaker}");
+            }
+        }
+
+        void SendMessage(string outMessage)
+        {
+            sendMessageQueue.Enqueue(outMessage);
+        }
+    }
+}
